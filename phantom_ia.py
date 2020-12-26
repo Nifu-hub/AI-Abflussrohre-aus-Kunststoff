aee5b7a03e3ed78c648e0835fba0066a544b4ac7
brihoumb <benjamin.brihoum@epitech.eu>
2020-12-26 16:21:44 +0100

    :monkey: I love python

--- phantom_ia.py
+++ phantom_ia.py
@@ -1,99 +1,73 @@
-import json
-import logging
 import os
-import random
+import json
 import socket
+import random
+import logging
 from logging.handlers import RotatingFileHandler
 
 import protocol
 
-host = "localhost"
-port = 12000
-# HEADERSIZE = 10
 
-"""
-    game data
-"""
-# determines whether the power of the character is used before
-# or after moving
-permanents = {"pink"}
-before = {"purple", "brown"}
-after = {"black", "white", "red", "blue", "grey"}
-
-mandatory_powers = ["red", "blue", "grey"]
-
-# Ghost info color, suspect, power
-ghosts_stats =  {"pink": {"suspect": False, "power": False},
-                "blue": {"suspect": False, "power": False},
-                "purple": {"suspect": False, "power": False},
-                "grey": {"suspect": False, "power": False},
-                "white": {"suspect": False, "power": False},
-                "black": {"suspect": False, "power": False},
-                "red": {"suspect": False, "power": False},
-                "brown": {"suspect": False, "power": False}}
-
-# Informations per case of the map
-pos = { 0: [],
-        1: [],
-        2: [],
-        3: [],
-        4: [],
-        5: [],
-        6: [],
-        7: [],
-        8: [],
-        9: []}
-
+'''
+    Global data.
+'''
 lock = []
-
-fantom = ""
-
+fantom = ''
+port = 12000
 carlotta = -1
+host = 'localhost'
+permanents = {'pink'}
+before = {'purple', 'brown'}
+mandatory_powers = ['red', 'blue', 'grey']
+after = {'black', 'white', 'red', 'blue', 'grey'}
+ghosts_stats = {'pink': {'suspect': False, 'power': False},
+                'blue': {'suspect': False, 'power': False},
+                'purple': {'suspect': False, 'power': False},
+                'grey': {'suspect': False, 'power': False},
+                'white': {'suspect': False, 'power': False},
+                'black': {'suspect': False, 'power': False},
+                'red': {'suspect': False, 'power': False},
+                'brown': {'suspect': False, 'power': False}}
 
-# ways between rooms
-# rooms are numbered
-# from right to left
-# from bottom to top
-# 0 ---> 9
+pos = {0: [], 1: [], 2: [], 3: [], 4: [],
+       5: [], 6: [], 7: [], 8: [], 9: []}
 passages = [{1, 4}, {0, 2}, {1, 3}, {2, 7}, {0, 5, 8},
             {4, 6}, {5, 7}, {3, 6, 9}, {4, 9}, {7, 8}]
-# ways for the pink character
 pink_passages = [{1, 4}, {0, 2, 5, 7}, {1, 3, 6}, {2, 7}, {0, 5, 8, 9},
                  {4, 6, 1, 8}, {5, 7, 2, 9}, {3, 6, 9, 1}, {4, 9, 5},
                  {7, 8, 4, 6}]
 
 
-"""
-set up fantom logging
-"""
+'''
+    Set up fantom logging handlers.
+'''
 fantom_logger = logging.getLogger()
-print(fantom_logger)
 fantom_logger.setLevel(logging.DEBUG)
 formatter = logging.Formatter(
-    "%(asctime)s :: %(levelname)s :: %(message)s", "%H:%M:%S")
-# file
-if os.path.exists("./logs/fantom.log"):
-    os.remove("./logs/fantom.log")
+    '%(asctime)s :: %(levelname)s :: %(message)s', '%H:%M:%S')
+
+os.path.exists('./logs/fantom.log') and os.remove('./logs/fantom.log')
 file_handler = RotatingFileHandler('./logs/fantom.log', 'a', 1000000, 1)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
 fantom_logger.addHandler(file_handler)
-# stream
+
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.WARNING)
 fantom_logger.addHandler(stream_handler)
 
 
 class Player():
-
+    '''
+        Player class manager.
+    '''
     def __init__(self):
-
+        self.step = 0
         self.end = False
+        self.next_rep = []
         self.nb_suspect = 0
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.step = 0
-        self.next_rep = []
 
     def connect(self):
         self.socket.connect((host, port))
@@ -119,10 +93,9 @@ class Player():
         return answer
 
     def answer(self, question):
-        # work
         print(question)
-        data = question["data"]
-        game_state = question["game state"]
+        data = question['data']
+        game_state = question['game state']
         if self.step == 1:
             response = self.calc_answer(question)
         else:
@@ -130,61 +103,54 @@ class Player():
             del self.next_rep[0]
         response_index = data.index(response)
         # log
-        fantom_logger.debug("|\n|")
-        fantom_logger.debug("fantom answers")
-        fantom_logger.debug(f"question type ----- {question['question type']}")
-        fantom_logger.debug(f"data -------------- {data}")
-        fantom_logger.debug(f"response index ---- {response_index}")
-        fantom_logger.debug(f"response ---------- {data[response_index]}")
-        print("------------------------------ REPONSE ------------------------------------")
+        fantom_logger.debug('|\n|')
+        fantom_logger.debug('fantom answers')
+        fantom_logger.debug(f'question type ----- {question['question type']}')
+        fantom_logger.debug(f'data -------------- {data}')
+        fantom_logger.debug(f'response index ---- {response_index}')
+        fantom_logger.debug(f'response ---------- {data[response_index]}')
+        print('----------------------- REPONSE -----------------------------')
         print(response)
         print(data)
         print(response_index)
-        print("---------------------------- END REPONSE ----------------------------------")
+        print('-------------------- END REPONSE ----------------------------')
         return response_index
 
     def handle_json(self, data):
         data = json.loads(data)
-
         print(data)
         print('step :')
         print(self.step)
-        if data["question type"] == 'select character':
+        if data['question type'] == 'select character':
             self.step = 1
         else:
             self.step = self.step + 1
-        #print('step 2')
-        # save data
-        carlotta = data["game state"]["position_carlotta"]
-        lock = data["game state"]["blocked"]
-        fantom = data["game state"]["fantom"]
+        carlotta = data['game state']['position_carlotta']
+        lock = data['game state']['blocked']
+        fantom = data['game state']['fantom']
         for x in pos:
             pos[x] = []
         self.nb_suspect = 0
-        for ghost in data["game state"]["characters"]:
-            ghosts_stats[ghost["color"]]["power"] = ghost["power"]
-            ghosts_stats[ghost["color"]]["suspect"] = ghost["suspect"]
-            if ghost["suspect"] == True:
+        for ghost in data['game state']['characters']:
+            ghosts_stats[ghost['color']]['power'] = ghost['power']
+            ghosts_stats[ghost['color']]['suspect'] = ghost['suspect']
+            if ghost['suspect'] is True:
                 self.nb_suspect += 1
-            pos[ghost["position"]].append(ghost["color"])
-        # get the answer
+            pos[ghost['position']].append(ghost['color'])
         response = self.answer(data)
-        # send back to server
-        bytes_data = json.dumps(response).encode("utf-8")
+        bytes_data = json.dumps(response).encode('utf-8')
         protocol.send_json(self.socket, bytes_data)
 
     def run(self):
-
         self.connect()
         while self.end is not True:
             received_message = protocol.receive_json(self.socket)
             if received_message:
                 self.handle_json(received_message)
             else:
-                print("no message, finished learning")
+                print('no message, finished learning')
                 self.end = True
 
 
 p = Player()
-
 p.run()
